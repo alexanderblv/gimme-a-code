f18b32a40071d15562b9bf4af2faf3bff611e996
Alexander B. <137616408+alexanderblv@users.noreply.github.com>
2025-03-04 10:48:20 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -40,7 +40,7 @@ function removeMole(id) {
 }
 
 function changeScore() {
-    document.getElementById('score').getElementsByTagName('span')[0].innerHTML =  score + ' codes ';
+    document.getElementById('score').getElementsByTagName('span')[0].innerHTML =  'Codes: ' + score;
 }
 
 function startTimer() {
